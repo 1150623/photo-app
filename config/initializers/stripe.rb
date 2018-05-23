41b20cf654c1375c16683615fbde86af8f129b44
Guilherme Ferreira <1150623@isep.ipp.pt>
2018-05-23 01:11:26 +0100

    gitignore update

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
-  :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
+  :publishable_key => 'pk_test_wvOyE1z0g2uox7TkKdPla6w0',
+  :secret_key => 'sk_test_QO3KseslV3h1QBR4UUCZUK8O'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
